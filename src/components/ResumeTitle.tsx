02a066a0aa142d38f69ee760e1beb6cf3ac250d7
sjoleee <cent7425@gmail.com>
2023-04-06 02:22:29 +0900

    fix: gradient 컬러 theme 사용

--- src/components/ResumeTitle.tsx
+++ src/components/ResumeTitle.tsx
@@ -2,7 +2,7 @@ import { DataProps } from "@/types";
 
 const ResumeTitle = ({ resumeTitle }: Pick<DataProps, "resumeTitle">) => {
   return (
-    <div className="bg-gradient-to-r from-[#51cf66] via-PRIMARY to-[#0c8599] h-2 sticky top-0 md:static md:h-20 flex justify-center items-center">
+    <div className="bg-gradient-to-r from-GRADIENT_FROM via-PRIMARY to-GRADIENT_TO h-2 sticky top-0 md:static md:h-20 flex justify-center items-center">
       <span className="text-2xl hidden md:block font-semibold text-white">{resumeTitle.title}</span>
     </div>
   );
