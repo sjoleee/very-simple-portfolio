2eddbdfc301d9ed7b5dcbdbf19b341769fa8437c
sjoleee <cent7425@gmail.com>
2023-04-06 02:21:48 +0900

    chore: 주석 작성

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,37 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      // NOTE: 본 템플릿은 대부분이 모노톤으로 이루어져 있습니다. primary 컬러만 수정하여 사용하시는 것을 권장드립니다.
       colors: {
         GRAY_LIGHT: "#f1f3f5",
         GRAY: "#adb5bd",
         GRAY_HEAVY: "#868e96",
         GRAY_EXTRAHEAVY: "#495057",
         BLACK: "#212529",
+        /**
+         * @description selection(드래그 블록)에 사용되는 컬러입니다.
+         */
         PRIMARY_LIGHT: "#c3fae8",
+
+        /**
+         * @description 자기소개의 이름 부분, code tag의 darkmode에 사용되는 컬러입니다.
+         */
         PRIMARY: "#12b886",
+
+        /**
+         * @description code tag, link hover icon에 사용되는 컬러입니다.
+         */
         PRIMARY_HEAVY: "#087f5b",
+
+        /**
+         * @description 페이지 최상단 gradient의 시작 컬러입니다.
+         */
+        GRADIENT_FROM: "#51cf66",
+
+        /**
+         * @description 페이지 최상단 gradient의 종료 컬러입니다.
+         */
+        GRADIENT_TO: "#0c8599",
       },
     },
   },
