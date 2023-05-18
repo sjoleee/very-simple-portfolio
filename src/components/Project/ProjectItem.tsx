f842153c3c0e16002e7423b0a6c5cf2dca6be003
asst2301075 <asst2301075@tosspartners.com>
2023-05-18 11:55:19 +0900

    feat: 줄바꿈 허용

--- src/components/Project/ProjectItem.tsx
+++ src/components/Project/ProjectItem.tsx
@@ -42,7 +42,7 @@ const ProjectItem = ({
       </div>
       <div className="md:border-GRAY_LIGHT md:border-solid md:border-l-[1px] md:pl-4 markdown flex flex-col w-full gap-2">
         <div>
-          <blockquote>{description}</blockquote>
+          <blockquote className="whitespace-pre-wrap">{`${description}`}</blockquote>
           <div className="flex gap-1 flex-wrap">
             {stack.map((stack) => (
               <span
